b59667b464c42d569456522526b7c80b67b89571
hialeen <hialeen@hotmail.com>
2026-04-13 09:43:31 -0300

    chore: Force new build hash

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
+    // Build timestamp: 2026-04-13T12:40:00
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   },
 })
